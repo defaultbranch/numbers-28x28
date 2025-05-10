781fc2b730ea7714b1dcc25bdd783f7af01fc3c3
Christian Fuchs <kontaktierbar@gmail.com>
2025-05-10 20:20:36 +0200

    add type annotations

--- src/main.py
+++ src/main.py
@@ -2,9 +2,16 @@ from mnist_dataloader import MnistDataloader
 from os import path
 import random
 import matplotlib.pyplot as plt
+from typing import List
+from array import array
+from numpy import ndarray
 
 
-def show_images(images, title_texts):
+def show_images(
+        images: List[ndarray],
+        title_texts: List[str]
+        ) -> None:
+
     cols = 5
     rows = int(len(images)/cols) + 1
     plt.figure(figsize=(30,20))
@@ -19,37 +26,44 @@ def show_images(images, title_texts):
         index += 1
     plt.show()
 
-def main():
+
+def main() -> None:
 
     input_path = 'mnist'
-    training_images_filepath = path.join(input_path, 'train-images.idx3-ubyte')
-    training_labels_filepath = path.join(input_path, 'train-labels.idx1-ubyte')
-    test_images_filepath = path.join(input_path, 't10k-images.idx3-ubyte')
-    test_labels_filepath = path.join(input_path, 't10k-labels.idx1-ubyte')
+    training_images_filepath: str = path.join(input_path, 'train-images.idx3-ubyte')
+    training_labels_filepath: str = path.join(input_path, 'train-labels.idx1-ubyte')
+    test_images_filepath: str = path.join(input_path, 't10k-images.idx3-ubyte')
+    test_labels_filepath: str = path.join(input_path, 't10k-labels.idx1-ubyte')
 
-    mnist_dataloader = MnistDataloader(
+    mnist_dataloader: MnistDataloader = MnistDataloader(
         training_images_filepath,
         training_labels_filepath,
         test_images_filepath,
         test_labels_filepath
         )
 
+    training_images: List[ndarray]
+    training_labels: array
     training_images, training_labels = mnist_dataloader.read_images_labels(training_images_filepath, training_labels_filepath)
+
+    test_images: List[ndarray]
+    test_labels: array
     test_images, test_labels = mnist_dataloader.read_images_labels(test_images_filepath, test_labels_filepath)
 
-    images_2_show = []
-    titles_2_show = []
+    images_2_show: List[ndarray] = []
+    titles_2_show: List[str] = []
     for i in range(0, 10):
-        r = random.randint(1, 60000)
+        r: int = random.randint(1, 60000)
         images_2_show.append(training_images[r])
         titles_2_show.append('training image [' + str(r) + '] = ' + str(training_labels[r]))
 
     for i in range(0, 5):
-        r = random.randint(1, 10000)
+        r: int = random.randint(1, 10000)
         images_2_show.append(test_images[r])
         titles_2_show.append('test image [' + str(r) + '] = ' + str(test_labels[r]))
 
     show_images(images_2_show, titles_2_show)
+
 
 if __name__ == "__main__":
     main()
