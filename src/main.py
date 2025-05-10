5215a0702bf34ada17c36709e5f2552fe37ad4ad
Christian Fuchs <kontaktierbar@gmail.com>
2025-05-10 20:02:39 +0200

    display some training data

--- src/main.py
+++ src/main.py
@@ -1,5 +1,23 @@
 from mnist_dataloader import MnistDataloader
 from os import path
+import random
+import matplotlib.pyplot as plt
+
+
+def show_images(images, title_texts):
+    cols = 5
+    rows = int(len(images)/cols) + 1
+    plt.figure(figsize=(30,20))
+    index = 1
+    for x in zip(images, title_texts):
+        image = x[0]
+        title_text = x[1]
+        plt.subplot(rows, cols, index)
+        plt.imshow(image, cmap=plt.cm.gray)
+        if (title_text != ''):
+            plt.title(title_text, fontsize = 15);
+        index += 1
+    plt.show()
 
 def main():
 
@@ -11,6 +29,20 @@ def main():
 
     mnist_dataloader = MnistDataloader(training_images_filepath, training_labels_filepath, test_images_filepath, test_labels_filepath)
     (x_train, y_train), (x_test, y_test) = mnist_dataloader.load_data()
+
+    images_2_show = []
+    titles_2_show = []
+    for i in range(0, 10):
+        r = random.randint(1, 60000)
+        images_2_show.append(x_train[r])
+        titles_2_show.append('training image [' + str(r) + '] = ' + str(y_train[r]))
+
+    for i in range(0, 5):
+        r = random.randint(1, 10000)
+        images_2_show.append(x_test[r])
+        titles_2_show.append('test image [' + str(r) + '] = ' + str(y_test[r]))
+
+    show_images(images_2_show, titles_2_show)
 
 if __name__ == "__main__":
     main()
